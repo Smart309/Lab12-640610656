ea9847e12523722d15eebe9987603cf8b5421761
Smart309 <pattharapon_takham@cmu.ac.th>
2021-12-21 15:47:02 +0700

    "UP ALL R1"

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,28 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<string>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+int main()
+{
+    cout << "Press Enter 3 times to reveal your future." ;
+    cin.get();
+    cin.get();
+    cin.get();
+    
+    srand(time(0)) ;
+    int num = rand() % 9 ;
+    string G ;
+    if(num == 0) G = "A" ;
+    else if(num == 1) G = "B+" ;
+    else if(num == 2) G = "B" ;
+    else if(num == 3) G = "C+" ;
+    else if(num == 4) G = "C" ;
+    else if(num == 5) G = "D+" ;
+    else if(num == 6) G = "D" ;
+    else if(num == 7) G = "F" ;
+    else if(num == 8) G = "W" ;
+    cout << "You will get " << G << " in this 261102." ;
+    return 0 ;
+}
